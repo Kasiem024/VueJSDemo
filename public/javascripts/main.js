cf3455711fdeee25ff4bc3f35c85d4e48dee6a27
Kasiem Saeed <kasiemsaeed@gmail.com>
2022-01-20 11:39:42 +0100

    vue works

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
 console.log('main.js is alive');
+
+window.onload = () => {
+
+    const Counter = {
+        data() {
+            return {
+                counter: 0
+            }
+        }
+    }
+
+    Vue.createApp(Counter).mount('#counter');
+}
